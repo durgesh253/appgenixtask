fd8f5cc644219e9b9071c32062ead18aafaf3681
durgesh253 <110898816+durgesh253@users.noreply.github.com>
2024-12-21 20:37:11 +0530

    Update urls.py
    second commit

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+
 
 urlpatterns = [
     # User-related views
     path('signup/', views.UserCreateView.as_view(), name='user-create'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('update/', views.UserUpdateView.as_view(), name='user-update'),
     path('delete/', views.UserDeleteView.as_view(), name='user-delete'),
     
